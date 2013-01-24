774cb91b5319c1268f9a0ed5b34722f1641bcef6
Ignacio Freiberg <hookdump@gmail.com>
2013-01-24 00:30:51 -0200

    Clean up core routes

--- apps/smsprep-core/routes.js
+++ apps/smsprep-core/routes.js
@@ -3,64 +3,7 @@ var User = require('../lib/models/user');
 exports.init = function(app, config, passport) {
 
   app.get('/', function(req, res) {
-
-    if (req.user) {
-
-      res.redirect('/dashboard');
-
-    } else {
-      // GUEST!
-      User.countUsers( function(err, count) {
-        if (err) {
-          count = "???";
-        }
-        res.render('index', { title: config.title, cur_section: "index", userCount: count });
-      });
-
-    }
-
-  });
-
-  app.get('/login', function(req, res) {
-    res.render('login', { title: config.title });
-  });
-
-  app.post('/login',
-    passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login', failureFlash: true } )
-  );
-
-  app.get('/logout', function(req, res){
-    req.logout();
-    res.redirect('/');
-  });
-
-  app.get('/join', function(req, res) {
-    res.render('join', { title: config.title });
-  });
-
-  app.post('/join', function(req, res) {
-    User.createUser( req.body.username, req.body.password, function(err, u, msg) {
-      if (err || !u) {
-        req.flash('error', msg.error);
-        res.redirect('/?err=1');
-      } else {
-        req.flash('success', msg.success);
-        res.redirect('/');
-      }
-    });
-  });
-
-  app.get('/dashboard', function(req, res) {
-    if (!req.user) {
-      res.redirect('/');
-    } else {
-        console.log("LOADING DASHBOARD");
-        var info = {};
-        User.getCurrentStreak(req.user.id, function(err, data) {
-          info.curStreak = data;
-          res.render('dashboard', { title: config.title, cur_section: "dashboard", info: info });  
-        });
-    }
+    res.json({success: true});
   });
 
   app.get('/msg/:phone/:message', function(req, res) {
@@ -82,7 +25,6 @@ exports.init = function(app, config, passport) {
       }
       
     });
-    
   });
 
   app.get('/api/start/:phone', function(req, res) {
