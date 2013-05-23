ee48c6da9eb312ca9f7d2bce00e6346fed0d4f2e
Ignacio Freiberg <hookdump@gmail.com>
2013-05-23 11:57:50 -0300

    bouncy changes

--- apps/bouncy/app.js
+++ apps/bouncy/app.js
@@ -6,7 +6,10 @@ var server = bouncy(function (req, res, bounce) {
     var reqHost = req.headers.host;
     var routed  = false;
 
+    console.log('** TRYING TO ROUTE: ' + reqHost);
+
     _(currentConf.services).each(function(service) {
+        console.log(' testing for: ', service);
         if (reqHost === service.domain) {
             routed = true;
             console.log('bouncing ' + service.domain + ' => ' + service.port);
--- conf/currentConf.js
+++ conf/currentConf.js
@@ -2,6 +2,7 @@ var servicesConf 	= require('./services.js');
 var connectionsConf = require('./connections.js');
 var env          	= process.env.NODE_ENV || 'development';
 
+console.log('@@@ loading configuration for environment ' + env);
 var currentConf = {
 	services: servicesConf[env]
 	, connections: connectionsConf[env]
