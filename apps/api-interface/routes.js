12a58ea718e0290729ea05c44240d90794d0f79b
Ignacio Freiberg <hookdump@gmail.com>
2013-05-23 12:15:24 -0300

    return queryReceived

--- apps/api-interface/routes.js
+++ apps/api-interface/routes.js
@@ -6,22 +6,22 @@ exports.init = function(app, config, lib) {
 
   // Student start
   app.get('/:partner/:uid/start', function(req, res) {
-    res.json({success: true});
+    res.json({success: true, queryReceived: req.query});
   });
 
   // Student edit
   app.get('/:partner/:uid/edit', function(req, res) {
-    res.json({success: true});
+    res.json({success: true, queryReceived: req.query});
   });
 
   // Student activate
   app.get('/:partner/:uid/activate', function(req, res) {
-    res.json({success: true});
+    res.json({success: true, queryReceived: req.query});
   });
 
   // Student deactivate
   app.get('/:partner/:uid/deactivate', function(req, res) {
-    res.json({success: true});
+    res.json({success: true, queryReceived: req.query});
   });
 
   // Student status check
@@ -39,34 +39,12 @@ exports.init = function(app, config, lib) {
 
   // Student reconfirmation
   app.get('/:partner/:uid/reconfirmation', function(req, res) {
-    res.json({success: true});
+    res.json({success: true, queryReceived: req.query});
   });
 
   // Custom message
   app.get('/:partner/:uid/send', function(req, res) {
-    res.json({success: true, messageToSend: req.query["message"]});
-  });
-
-
-  app.get('/msg/:phone/:message', function(req, res) {
-    var phone = req.params.phone;
-    var msg = req.params.message;
-
-    console.log("Incoming message from student " + phone + ": " + msg);
-    lib.Student.findOne({ phone: phone }, function(err, student) {
-      if (err) {
-        console.log("ERR:");
-        console.log(err);
-        res.json({success: false, error: err.toString()});
-      } else {
-        if (student) {
-          res.json({success: true});
-        } else {
-          res.json({success: false, error: "Student " + phone + " does not exist!"});
-        }
-      }
-      
-    });
+    res.json({success: true, queryReceived: req.query});
   });
 
 }
