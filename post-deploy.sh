a2f95a0001485b010999162e0f335f7bf5e1ca03
Ignacio Freiberg <hookdump@gmail.com>
2013-05-24 14:11:20 -0300

    quick deploy

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,7 +1,7 @@
-make uninstall
+# make uninstall
 make install
 
-make core ENV=$1
-make sms ENV=$1
-make web ENV=$1
-make api ENV=$1
+# make core ENV=$1
+# make sms ENV=$1
+# make web ENV=$1
+# make api ENV=$1
