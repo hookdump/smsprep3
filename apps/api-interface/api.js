4814700b31794152d81ab0945375a0a48135f504
Ignacio Freiberg <hookdump@gmail.com>
2013-06-02 14:09:02 -0300

    better abstraction in the API module

--- apps/api-interface/api.js
+++ apps/api-interface/api.js
@@ -1,9 +1,116 @@
-var ApiExport = {};
+var ApiExport = {
+	config: null
+	, lib: null
+};
 
 var _       = require('underscore');
 var Step    = require('step');
 
-ApiExport.noop = function() {
+ApiExport.init = function(config, lib) {
+	ApiExport.config = config;
+	ApiExport.lib	= lib;
+};
+
+ApiExport.Student = {
+	upsert: function(studentParams, studentData, method, cb) {
+		var findQuery = ApiExport.lib.Utils.buildFindQuery( studentParams );
+		ApiExport.lib.Student.upsertStudent( findQuery , studentData , method, function(err, updatedStudent) {
+			log.error('upserting student', err);
+
+			var sendBack = {success: true};
+			if (err) {
+				sendBack['success'] = false;
+				sendBack['errors'] = err.errors;
+			} else {
+				sendBack['studentId'] = updatedStudent._id;
+			}
+
+			return cb(err, sendBack);
+		});
+	},
+	start: function(studentParams, studentData, callback) {
+		log.apiMethod('student.start');
+		ApiExport.Student.upsert(studentParams, studentData, 'start', callback);
+	},
+	edit: function(studentParams, studentData, callback) {
+		log.apiMethod('student.edit');
+		ApiExport.Student.upsert(studentParams, studentData, 'edit', callback);
+	},
+
+	changeActive: function(studentParams, activate, cb) {
+		var findQuery = ApiExport.lib.Utils.buildFindQuery( studentParams );
+		ApiExport.lib.Student.activateStudent( findQuery , activate , function(err, affected) {
+			log.error('de/activating student', err);
+
+			var sendBack = {success: true};
+			if (affected === 0) {
+				sendBack['success'] = false;
+				sendBack['errors'] = ['the student ' + findQuery.externalId + ' does not exist in our database!']; 
+			}
+			if (err) {
+				sendBack['success'] = false;
+				sendBack['errors'] = ['database error while activating student'];
+			}
+
+			return cb(err, sendBack);
+		});
+	},
+	activate: function(studentParams, callback) {
+		log.apiMethod('student.activate');
+		ApiExport.Student.changeActive(studentParams, true, callback);
+	},
+	deactivate: function(studentParams, callback) {
+		log.apiMethod('student.deactivate');
+		ApiExport.Student.changeActive(studentParams, false, callback);
+	},
+
+	status: function(studentParams, callback) {
+		log.apiMethod('student.status');
+		var findQuery 	= ApiExport.lib.Utils.buildFindQuery( studentParams );
+		var sendBack 	= {success: true};
+
+		ApiExport.lib.Student.loadData( findQuery, function(err, myStudent) {
+
+			if (myStudent) {
+
+				var retStatus = {
+					studentId: myStudent._id
+					, active: myStudent.active || 'true'
+					, externalId: myStudent.externalId
+					, confirmed: myStudent.confirmed || 'false'
+					, created: myStudent.joined
+					, schedule: myStudent.schedule
+					, lessons: myStudent.lessons
+					, lessongroups: myStudent.lessongroups
+					, stats: {
+						totalAnswers: 0
+						, correctAnswers: 0
+					}
+				};
+				sendBack['status'] = retStatus;
+
+			} else {
+
+				sendBack['success'] = false;
+				sendBack['errors'] = ['the student ' + findQuery.externalId + '@' + findQuery.partner + ' does not exist in our database!']; 
+
+			}
+
+			callback(err, sendBack);
+
+		});
+
+	},
+
+	reconfirm: function(studentParams, callback) {
+		log.apiMethod('student.reconfirm');
+		callback({success: true});
+	},
+
+	sendMessage: function(studentParams, callback) {
+		log.apiMethod('student.send');
+		callback({success: true});
+	},
 
 };
 
