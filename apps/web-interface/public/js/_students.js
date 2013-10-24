d576815ccb26eb22b832ae730e70524e5cf59217
Ignacio Freiberg <hookdump@gmail.com>
2013-10-24 08:57:04 -0300

    admin changes

--- apps/web-interface/public/js/_students.js
+++ apps/web-interface/public/js/_students.js
@@ -1,8 +1,15 @@
 $(function () {
 
 	var init = function() {
-		alert('asd');
 	};
+
+	$('.del_student').click(function(ev) {
+		ev.preventDefault();
+		var answer = confirm("Are you sure you want to delete this user?");
+		if (!answer) {
+			return false;
+		}
+	});
 
 	init();
 
--- conf/services.js
+++ conf/services.js
@@ -1,64 +1,64 @@
-// Instance: eprep
+// Instance: smsprep
 // Config: services
 
 module.exports = {
 	test: {
 		'web': {
-			domain: 'sms.eprep.local'
+			domain: 'smsprep.local'
 			, port: 8200
 		}, 
 		'core': {
-			domain: 'core.sms.eprep.local'
+			domain: 'core.smsprep.local'
 			, port: 8201
 		},
 		'sms': {
-			domain: 'sms.sms.eprep.local'
+			domain: 'sms.smsprep.local'
 			, port: 8202
 		},
 		'api': {
-			domain: 'api.sms.eprep.local'
+			domain: 'api.smsprep.local'
 			, port: 8203
 		}
 	},
 	development: {
 		'web': {
-			domain: 'sms.eprep.local'
+			domain: 'smsprep.local'
 			, port: 8200
 		}, 
 		'core': {
-			domain: 'core.sms.eprep.local'
+			domain: 'core.smsprep.local'
 			, port: 8201
 		},
 		'sms': {
-			domain: 'sms.sms.eprep.local'
+			domain: 'sms.smsprep.local'
 			, port: 8202
 		},
 		'api': {
-			domain: 'api.sms.eprep.local'
+			domain: 'api.smsprep.local'
 			, port: 8203
 		}
 	},
 	staging: {
 		'web': {
-			domain: 'staging.sms.eprep.com'
+			domain: 'staging.smsprep.com'
 			, port: 8100
 		}, 
 		'core': {
-			domain: 'core.staging.sms.eprep.com'
+			domain: 'core.staging.smsprep.com'
 			, port: 8101
 		},
 		'sms': {
-			domain: 'sms.staging.sms.eprep.com'
+			domain: 'sms.staging.smsprep.com'
 			, port: 8102
 		},
 		'api': {
-			domain: 'api.staging.sms.eprep.com'
+			domain: 'api.staging.smsprep.com'
 			, port: 8103
 		}
 	},
 	production: {
 		'watchmen-web': {
-			domain: 'status.sms.eprep.com'
+			domain: 'status.smsprep.com'
 			, port: 3003
 		}, 
 		'loopback-ping': {
@@ -67,40 +67,40 @@ module.exports = {
 		}, 
 
 		'web-www': {
-			domain: 'www.sms.eprep.com'
+			domain: 'www.smsprep.com'
 			, port: 8000
 		}, 
 		'web': {
-			domain: 'sms.eprep.com'
+			domain: 'smsprep.com'
 			, port: 8000
 		}, 
 		'core': {
-			domain: 'core.sms.eprep.com'
+			domain: 'core.smsprep.com'
 			, port: 8001
 		},
 		'sms': {
-			domain: 'sms.sms.eprep.com'
+			domain: 'sms.smsprep.com'
 			, port: 8002
 		},
 		'api': {
-			domain: 'api.sms.eprep.com'
+			domain: 'api.smsprep.com'
 			, port: 8003
 		},
 
 		'web-staging': {
-			domain: 'staging.sms.eprep.com'
+			domain: 'staging.smsprep.com'
 			, port: 8100
 		}, 
 		'core-staging': {
-			domain: 'core.staging.sms.eprep.com'
+			domain: 'core.staging.smsprep.com'
 			, port: 8101
 		},
 		'sms-staging': {
-			domain: 'sms.staging.sms.eprep.com'
+			domain: 'sms.staging.smsprep.com'
 			, port: 8102
 		},
 		'api-staging': {
-			domain: 'api.staging.sms.eprep.com'
+			domain: 'api.staging.smsprep.com'
 			, port: 8103
 		}
 
