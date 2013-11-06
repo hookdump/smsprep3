42405e90a5aed2b2a3d744fdc750e584796d1b6b
Ignacio Freiberg <hookdump@gmail.com>
2013-11-06 14:11:51 -0300

    signup landing

--- apps/web-interface/controllers/users.js
+++ apps/web-interface/controllers/users.js
@@ -1,4 +1,4 @@
-var request = require('request');
+var requestJSON = require('request-json');
 
 var usersController = function(app, config, lib, passport) {
   log.loading('users controllers');
@@ -9,26 +9,47 @@ var usersController = function(app, config, lib, passport) {
       res.redirect('/admin');
     } else {
       // res.render('index', { title: config.title, cur_section: "index" });
-      console.log( 'success - ' + req.query.success );
-      res.render('landing', { layout: 'clean_layout', title: lib.Config.name, cur_section: 'landing' });
+      var allowedFirstDigit = (lib.Config.env != 'production') ? '[19]' : '[1]';
+      res.render('landing', { layout: 'clean_layout', title: lib.Config.name, cur_section: 'landing', allowed_first_digit: allowedFirstDigit });
     }
   });
 
   // Landing signup -------------------
   app.post('/landing/signup', function(req,res) {
     var phone = req.body.phonenumber;
+    var course = req.body.course;
+    var schedule = req.body.schedule;
+    var timezone = req.body.timezone;
 
-    // Create user
-    var myUrl     = 'http://api.smsprep.com/smsprep/' + phone + '/start'; 
+    console.log(phone + ' - ' + course + ' - ' + schedule + ' - ' + timezone);
+
+    var apiRoute = lib.Config.services['api']['domain'];
+    var myUrl     = 'http://' + apiRoute + '/'; 
+    var myUrlParams = 'smsprep/' + phone + '/start';
+
+    var groups = '';
+    if (course == 'SAT') {
+      var groups = 'SAT_MATH+SAT_READING+SAT_WRITING+';
+    }
+    if (course == 'ACT') {
+      var groups = 'ACT_READING+ACT_ENGLISH+ACT_MATH';
+    }
+
     var myBody    = {
-      
+      phone: phone,
+      lessongroups: groups,
+      timezone: timezone,
+      schedule: schedule
     };
+    myBody2 = JSON.stringify(myBody);
 
-    request.post('http://service.com/upload', myBody, function() {
-
+    console.log('requesting...');
+    var requestClient = requestJSON.newClient(myUrl);
+    requestClient.post(myUrlParams, myBody, function(err2, res2, body2) {
+      console.log(res.statusCode);
+      res.redirect('/?success=1');  
     });
-
-    res.redirect('/?success=1');
+    
   });
 
   // Login ----------------------------
